c6efb754b2224707dfef0081d63c4b655605b46c
马铭锋 <mamf2@ziroom.com>
2017-03-29 14:30:25 +0800

    新增mock方法

--- executer-web/src/test/java/TestMockController.java
+++ executer-web/src/test/java/TestMockController.java
@@ -1,11 +1,16 @@
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -16,15 +21,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
  * Desc :
  */
 @RunWith(value = SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:/spring-xml/applicationContext.xml")
+@ContextConfiguration("classpath*:executer-servlet.xml")
+@WebAppConfiguration
 public class TestMockController extends AbstractJUnit4SpringContextTests{
 
     Logger logger = LoggerFactory.getLogger(TestMockController.class);
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
     MockMvc mockMvc;
 
     @Test
     public void mockTest() throws Exception {
-          mockMvc.perform(get("http://www.baidu.com"))
+          mockMvc.perform(get("/api/queryPage"))
           .andDo(print());
     }
 }
