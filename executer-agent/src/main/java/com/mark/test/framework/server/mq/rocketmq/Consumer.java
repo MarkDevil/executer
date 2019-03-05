d5a9776b457bb0873c8afd8e4dd2e2668e057e93
Mark <mamingfeng007@gmail.com>
2019-03-05 23:44:25 +0800

    修改

---
+++ executer-agent/src/main/java/com/mark/test/framework/server/mq/rocketmq/Consumer.java
@@ -0,0 +1,35 @@
+package com.mark.test.framework.server.mq.rocketmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+
+/**
+ * Created by mark .
+ * Data   : 2019/3/5
+ */
+
+public class Consumer {
+
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+        // Instantiate with specified consumer group name.
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("mark");
+
+        // Specify name server addresses.
+        consumer.setNamesrvAddr("localhost:9876");
+
+        // Subscribe one more more topics to consume.
+        consumer.subscribe("Topic-1", "*");
+        // Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+
+        //Launch the consumer instance.
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
+}
---
+++ executer-agent/src/main/java/com/mark/test/framework/server/mq/rocketmq/SyncProducer.java
@@ -0,0 +1,34 @@
+package com.mark.test.framework.server.mq.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * Created by mark .
+ * Data   : 2019/3/5
+ */
+
+public class SyncProducer {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new DefaultMQProducer("mark");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("localhost:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("Topic-1" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult.toString());
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
